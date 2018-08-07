4662e1cb82edeef0c711c139a4cc9628c89b53e4
srdesai <isriramdesai@gmail.com>
2018-08-07 14:06:51 +0530

    adding 0's for sets games and points.

--- src/TennisMatch.java
+++ src/TennisMatch.java
@@ -11,6 +11,11 @@ public class TennisMatch {
         sets = new ArrayList<>(2);
         games = new ArrayList<>(2);
         points = new ArrayList<>(2);
+        for(int i=0;i<2;i++) {
+            sets.add(0);
+            games.add(0);
+            points.add(0);
+        }
     }
 
     public void displayScoreBoard(String matchStatus) {
